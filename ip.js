04e4eaa16e901aa5597e0c98945931e7f86da2bd
MuTu <59136159+githubdulong@users.noreply.github.com>
2021-01-05 14:45:15 +0800

    Update ip.js

--- ip.js
+++ ip.js
@@ -51,7 +51,7 @@ var emoji = flags.get(obj['country'])? flags.get(obj['country']):"🏴‍☠️"
 emoji=City_ValidCheck(obj['province']) == "香港"? "🇭🇰️":emoji
 emoji=City_ValidCheck(obj['province']) == "澳门"? "️🇲🇴️":emoji
 // emoji=City_ValidCheck(obj['province']) == "台湾"? "️️":emoji
-var title =  emoji +'『'+ City_ValidCheck(obj['province'])+'』';//+Area_check(obj['country']);
+var title =  emoji +''+ City_ValidCheck(obj['province'])+'';//+Area_check(obj['country']);
 var subtitle =  "  "+ ISP_ValidCheck(obj['isp']) + " ➠ "+ obj['country'];
 var ip = obj['addr']; 
 var description = '服务商:'+obj['isp'] + '\n'+'定位: [' +obj["latitude"]+","+obj["longitude"]+"]"+ '\n' + 'IP:'+ obj['addr'] + '\n' +'时区:'+ obj['timezone'];
