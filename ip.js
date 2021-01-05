990c8eac608c784b7ab325d95e22787a3e8d4e82
MuTu <59136159+githubdulong@users.noreply.github.com>
2021-01-05 20:47:25 +0800

    Update ip.js

--- ip.js
+++ ip.js
@@ -41,7 +41,7 @@ var flags = new Map([[ "AC" , "🇦🇨" ] , [ "AF" , "🇦🇫" ] , [ "AI" , "�
 var body = $response.body;
 var obj = JSON.parse(body);
 var title =flags.get(obj['countryCode']) + ' '+ City_ValidCheck(obj['city']);//+Area_check(obj['country']);
-var subtitle = ISP_ValidCheck(obj['org']);
+var subtitle =  " "+ ISP_ValidCheck(obj['isp']) + " ➠ "+ obj['country'];
 var ip = obj['query'];
 var description = '服务商:'+obj['isp'] + '\n'+'地区:' +City_ValidCheck(obj['regionName'])+ '\n' + 'IP:'+ obj['query'] + '\n' +'时区:'+ obj['timezone'];
 $done({title, subtitle, ip, description});
