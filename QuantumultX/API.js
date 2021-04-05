6083fcb66ac802317c75e31e2c4388406e7d8940
MuTu <59136159+githubdulong@users.noreply.github.com>
2021-04-05 09:01:45 +0800

    Update and rename TASK.json to API.js

---
+++ QuantumultX/API.js
@@ -0,0 +1,13 @@
+if ($response.statusCode != 200) {
+  $done(Null);
+}
+
+var body = $response.body;
+var obj = JSON.parse(body);
+var title = obj['country'];
+var subtitle = obj['city'] + ' ' + obj['isp'];
+var ip = obj['query'];
+var description = "国家" + ":" + obj['country'] + '\n' + "城市" + ":" + obj['city'] + '\n' + "运营商" + ":" + obj['isp'] + '\n' + "数据中心" + ":" + obj['org'];
+
+
+$done({title, subtitle, ip, description});
