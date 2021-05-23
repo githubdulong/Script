6db1e74103e1afc13fa19e3fedce3300f5ea2c73
MuTu <59136159+githubdulong@users.noreply.github.com>
2021-05-23 22:39:57 +0800

    Update mix.js

--- mix.js
+++ mix.js
@@ -1,6 +1,9 @@
-//转自越南老哥作品
-//
-surge：
+/*
+转自越南老哥作品
+=======Quantumult X=======
+#MIX解锁
+
+========Surge==========
 [Script]
 camera360-Mix = requires-body=1,script-path= https://raw.githubusercontent.com/githubdulong/Script/master/mix.js,type=http-response,pattern= ^https:\/\/bmall\.camera360\.com\/api\/mix\/recovery$
 
