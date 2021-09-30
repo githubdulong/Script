005c1ae45911871c1ab5ce52f872a03d055ef2c6
MuTu <59136159+githubdulong@users.noreply.github.com>
2021-09-30 16:41:36 +0800

    Update geo_location.js

--- geo_location.js
+++ geo_location.js
@@ -34,7 +34,7 @@ function json2info(cnt,paras) {
   cnt =JSON.parse(cnt)
   for (i=0;i<paras.length;i++) {
     res = cnt[paras[i]]? res + paran[i] + " : " + cnt[paras[i]] : res
-    res = paras[i] == "country_code"? res+" "+flags.get(cnt[paras[i]].toUpperCase())+"\n\n":res+"\n\n"
+    res = paras[i] == "country_code"? res+" "+flags.get(cnt[paras[i]].toUpperCase())+"":res+""
   }
   res =res+ ""
   return res
