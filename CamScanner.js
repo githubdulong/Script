82d17ac07010d944037293aff0913070dfcfbe80
MuTu <githubdulong@users.noreply.github.com>
2021-11-03 18:39:32 +0800

    Script

---
+++ CamScanner.js
@@ -0,0 +1,34 @@
+/*
+CamScanner 解锁部分高级特权
+
+***************************
+Quantumult X:
+
+[rewrite_local]
+^https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/CamScanner.js
+
+[mitm]
+hostname = ap*.intsig.net
+
+***************************
+Surge4 or Loon:
+
+[Script]
+http-response https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/CamScanner.js
+
+[MITM]
+hostname = ap*.intsig.net
+
+***************************
+Quantumult:  
+
+[REWRITE]
+https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? url simple-response SFRUUC8xLjEgMjAwIE9LCgp7CiAiZGF0YSI6IHsKICAicHNubF92aXBfcHJvcGVydHkiOiB7CiAgICJleHBpcnkiOiAiMTY0MzczMTIwMCIKICB9CiB9Cn0=
+
+[MITM]
+hostname = ap*.intsig.net
+
+**************************/
+let obj = JSON.parse($response.body);
+obj = {"data":{"psnl_vip_property":{"expiry":"8876965487"}}};
+$done({body: JSON.stringify(obj)});
