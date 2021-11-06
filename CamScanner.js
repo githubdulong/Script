1679a8fa228c48b9894407ab3eb427d5196c736f
MuTu <githubdulong@users.noreply.github.com>
2021-11-06 12:02:56 +0800

    CamScanner.js
    完善CamScanner解锁脚本

--- CamScanner.js
+++ CamScanner.js
@@ -5,7 +5,7 @@ CamScanner 解锁部分高级特权
 Quantumult X:
 
 [rewrite_local]
-^https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/CamScanner.js
+^https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? url script-response-body https://raw.githubusercontent.com/githubdulong/Script/master/CamScanner.js
 
 [mitm]
 hostname = ap*.intsig.net
@@ -14,21 +14,35 @@ hostname = ap*.intsig.net
 Surge4 or Loon:
 
 [Script]
-http-response https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/CamScanner.js
+http-response https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/githubdulong/Script/master/CamScanner.js
 
 [MITM]
 hostname = ap*.intsig.net
-
-***************************
-Quantumult:  
-
-[REWRITE]
-https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? url simple-response SFRUUC8xLjEgMjAwIE9LCgp7CiAiZGF0YSI6IHsKICAicHNubF92aXBfcHJvcGVydHkiOiB7CiAgICJleHBpcnkiOiAiMTY0MzczMTIwMCIKICB9CiB9Cn0=
 
 [MITM]
 hostname = ap*.intsig.net
 
 **************************/
-let obj = JSON.parse($response.body);
-obj = {"data":{"psnl_vip_property":{"expiry":"8876965487"}}};
-$done({body: JSON.stringify(obj)});
+let body = JSON.parse($response.body)
+    body.data.psnl_vip_property = {"renew_method": "appstore",
+      "initial_tm": "1614867690",
+      "svip": 1,
+      "auto_renewal": true,
+      "ms_first_pay": 0,
+      "pending": 0,
+      "group2_paid": 0,
+      "inherited_flag": 0,
+      "nxt_renew_tm": "9915126887",
+      "level_info": {
+        "level": 1,
+        "days": 1,
+        "end_days": 30
+      },
+      "group1_paid": 1,
+      "ys_first_pay": 0,
+      "renew_type": "year",
+      "expiry": 8487890487,
+      "grade": 2,
+      "last_payment_method": "appstore",
+      "product_id": "com.intsig.camscanner.premiums.oneyear.autorenewable.svip.low"}
+    $done({body:JSON.stringify(body)})
