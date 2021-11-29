db2877c27473eb55acf0f75b393bce14b138dfa9
MuTu <githubdulong@users.noreply.github.com>
2021-11-29 16:19:52 +0800

    price.js

--- qxmain.js
+++ qxmain.js
@@ -27,13 +27,14 @@ const ScriptVersion = 7;
 const ScriptUrl = `https://service.2ti.st/QuanX/Script/${ScriptIdentifier}`
 
 const res = $request;
-//const resp = isundefined($response) ? null : $response;
+
 let resp = null;
 try{
   resp =$response
 }catch(err){
   console.log(err)
 };
+
 let Status = {
     Enable: 1,
     Disable: 2
