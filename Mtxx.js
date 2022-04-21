b4fb25241158a4119fc1292dbe9cac312af56870
MuTu <githubdulong@users.noreply.github.com>
2022-04-21 10:17:55 +0800

    Mtxx.js
    替换美图秀秀未加密脚本

--- Mtxx.js
+++ Mtxx.js
@@ -3,12 +3,12 @@ QX
 [rewrite_local]
 ^https?://(api|h5).xiuxiu.meitu.com/(v1/user/show.json|v1/vip/vip_show.json|v1/vip/prompt/query.json|v1/h5/vip/sub_detail.json|v1/h5/user/self_show.json|v1/h5/vip/user_detail.json|v1/vip/prompt/query.json|v1/vip/prompt/query.json) url script-response-body https://raw.githubusercontent.com/githubdulong/Script/master/Mtxx.js
 [MITM]
-hostname: *.xiuxiu.meitu.com
+hostname: api.xiuxiu.meitu.com, h5.xiuxiu.meitu.com
 Surge
 [Script]
 美图秀秀 = requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/githubdulong/Script/master/Mtxx.js,type=http-response,pattern=^https?://(api|h5).xiuxiu.meitu.com/(v1/user/show.json|v1/vip/vip_show.json|v1/vip/prompt/query.json|v1/h5/vip/sub_detail.json|v1/h5/user/self_show.json|v1/h5/vip/user_detail.json|v1/vip/prompt/query.json|v1/vip/prompt/query.json)
 [MITM]
-hostname: *.xiuxiu.meitu.com
+hostname: api.xiuxiu.meitu.com, h5.xiuxiu.meitu.com
 
 原脚本：https://iosgs.xyz/gzFile/quanX/mtxx.conf
 */
@@ -16,7 +16,7 @@ hostname: *.xiuxiu.meitu.com
 var obj = JSON.parse($response.body);
 
 obj.data.vip_type=1;
-obj.data.expire_days=-99999999999;
+obj.data.expire_days=-9999999999;
 obj.data.screen_name="MuTu";
 obj.data.is_expire=0;
 obj.data.in_valid_time=5576488923;
