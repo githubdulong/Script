fa02bd985bc8dc7d95c2ad4c9d924343a4ee18d0
MuTu <59136159+githubdulong@users.noreply.github.com>
2022-10-10 00:20:02 +0800

    api.revenuecat.com

---
+++ revenuecat.js
@@ -0,0 +1,20 @@
+re('"expires_date":"\\w{4}@"period_type":"\\w+"','"expires_date":"2099@"period_type":"active"');
+function re() {
+var body = $response.body;
+if(!body){
+$done({});
+}
+ if (arguments[0].includes("@")) {
+  var regs = arguments[0].split("@");
+  var strs = arguments[1].split("@");
+  for (i = 0;i < regs.length;i++) {
+   var reg = new RegExp(regs[i],"g");
+   body = body.replace(reg, strs[i]);
+ }
+}
+ else {
+  var reg = new RegExp(arguments[0],"g");
+  body = body.replace(reg, arguments[1]);
+}
+ $done({body});
+} 
