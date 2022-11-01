d72359f1f17a9a82df0fae6ef7e0be557a9cb7c0
MuTu <59136159+githubdulong@users.noreply.github.com>
2022-11-01 14:09:19 +0800

    Douban.js
    恢复解析网

--- Douban.js
+++ Douban.js
@@ -9,11 +9,11 @@
     使用说明
  【Surge】
         [Script]
-        // 茶杯狐、大师兄影视、555电影
+        // 茶杯狐、大师兄影视、解析网、555电影
         豆瓣电影 = type=http-response,pattern=^https://m.douban.com/movie/subject/.+,requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/githubdulong/Script/master/Douban.js
   【quan_x】
         [rewrite_local]
-        // 茶杯狐、大师兄影视、555电影
+        // 茶杯狐、大师兄影视、解析网、555电影
        ^https://m.douban.com/movie/subject/.+ url script-response-body https://raw.githubusercontent.com/githubdulong/Script/master/Douban_qx.js
         
 
@@ -49,6 +49,7 @@ async function douban_addons() {
         
         let mweb = [`<a href="https://www.cupfox.com/search?key=${title[1]}">\t<img src="https://raw.githubusercontent.com/githubdulong/Script/master/Images/Chb.png" height="26" width="26" style="vertical-align: text-top;" /></a>`]
         mweb.push(`<a href="https://dsxys.com/sb/ke7nhZe3c1-.html?wd=${title[1]}&submit="><img src="https://raw.githubusercontent.com/githubdulong/Script/master/Images/Dsx.png" height="26" width="26" style="vertical-align: text-top;" /></a>`)
+        mweb.push(`<a href="https://im1907.top/?jx=${title[1]}&submit="><img src="https://raw.githubusercontent.com/githubdulong/Script/master/Images/Jxw.png" height="26" width="26" style="vertical-align: text-top;" /></a>`)
         mweb.push(`<a href="https://www.o8tv.com/vodsearch/-------------.html?wd=${title[1]}&submit="><img src="https://raw.githubusercontent.com/githubdulong/Script/master/Images/555.png" height="26" width="26" style="vertical-align: text-top;" /></a>`)
             let douban_options = {
         url: `https://frodo.douban.com/api/v2/movie/${movieId[1]}?apiKey=0ac44ae016490db2204ce0a042db2916`,
