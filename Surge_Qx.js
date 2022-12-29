6fcad8e50d609d7e2ea2bc7e5e3d518ff0b026ac
MuTu <du1238888@gmail.com>
2022-12-29 11:26:15 +0800

    Surge_Qx.js

--- Surge_Qx.js
+++ Surge_Qx.js
@@ -1,5 +1,5 @@
 let req = $request.url.replace(/Surge$/,'')
-let name = '#!name= ' + req.match(/.+\/(.+)\.(conf|js)/)?.[1] || '无名'
+let name = '#!name= ' + req.match(/.+\/(.+)\.(conf|js)/)?.[1] || '无名';
 !(async () => {
   let body = await http(req);
 
@@ -13,16 +13,18 @@ let MITM = "";
 
 body.forEach((x, y, z) => {
 	let type = x.match(
-		/script-|enabled=|reject|echo-response|url\srequest-header|hostname|url\s(302|307)|\s(request|response)-body/
+		/script-|enabled=|reject|echo-response|\-header|hostname|url\s(302|307)|\s(request|response)-body/
 	)?.[0];
 	if (type) {
 		switch (type) {
 			case "script-":
+			if (x.match('echo')) {throw '脚本不支持通用'}
 				z[y - 1]?.match("#") && script.push(z[y - 1]);
+				let proto = x.match('proto.js') ? ',binary-body-mode=1' : '';
 				script.push(
 					x.replace(
 						/(\^?http[^\s]+)\surl\sscript-(response|request)[^\s]+\s(http.+\/(.+)\.js)/,
-						'$4 = type=http-$2,pattern=$1,requires-body=1,max-size=0,script-path=$3,script-update-interval=0',
+						`$4 = type=http-$2,pattern=$1,requires-body=1${proto},max-size=0,script-path=$3,script-update-interval=0`,
 					),
 				);
 				break;
@@ -57,14 +59,24 @@ body.forEach((x, y, z) => {
 				URLRewrite.push(x.replace(/(\^?http[^\s]+).+/, "$1 - reject"));
 				break;
 
-			case "url request-header":
-				z[y - 1]?.match("#") && HeaderRewrite.push(z[y - 1]);
+			case "-header":
+			if (x.match(/\(\\r\\n\)/g).length === 2){			
+				z[y - 1]?.match("#") &&  HeaderRewrite.push(z[y - 1]);
+let op = x.match(/\sresponse-header/) ?
+'http-response ' : '';
+     if(x.match(/\$1\$2/)){
+		  HeaderRewrite.push(x.replace(/(\^?http[^\s]+).+?n\)([^\:]+).+/,`${op}$1 header-del $2`))	
+		}else{
 				HeaderRewrite.push(
 					x.replace(
-						/(\^?http[^\s]+)[^\)]+\)([^:]+):([^\(]+).+:\s(.+)\$2/,
-						"$1 header-replace-regex $2 $3 $4",
+						/(\^?http[^\s]+)[^\)]+\)([^:]+):([^\(]+).+\$1\s?\2?\:?([^\$]+)?\$2/,
+						`${op}$1 header-replace-regex $2 $3 $4''`,
 					),
 				);
+				}
+				}else{
+	$notification.post('不支持这条规则转换,已跳过','',`${x}`);
+				}
 				break;
 
 			case "echo-response":
@@ -91,13 +103,13 @@ body.forEach((x, y, z) => {
 	}
 }); //循环结束
 
-script = script.join("\n") && `[Script]\n${script.join("\n")}`;
+script = (script[0] || '') && `[Script]\n${script.join("\n")}`;
 
-URLRewrite = URLRewrite.join("\n") && `[URL Rewrite]\n${URLRewrite.join("\n")}`;
+URLRewrite = (URLRewrite[0] || '') && `[URL Rewrite]\n${URLRewrite.join("\n")}`;
 
-HeaderRewrite = HeaderRewrite.join("\n") && `[Header Rewrite]\n${HeaderRewrite.join("\n")}`;
+HeaderRewrite = (HeaderRewrite[0] || '') && `[Header Rewrite]\n${HeaderRewrite.join("\n")}`;
 
-MapLocal = MapLocal.join("\n") && `[MapLocal]\n${MapLocal.join("\n")}`;
+MapLocal = (MapLocal[0] || '') && `[MapLocal]\n${MapLocal.join("\n")}`;
 
 body = `${name}
 
@@ -111,7 +123,13 @@ ${MITM}`.replace(/\;/g,'#')
 
  $done({ response: { status: 200 ,body:body } });
 
-})();
+})()
+.catch((e) => {
+		$notification.post(`${e}`,'','');
+		$done()
+	})
+
+
 
 
 function http(req) {
