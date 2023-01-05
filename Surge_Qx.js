75c13bfe74aa9df06cdeb848f78f2e76605433ff
MuTu <du1238888@gmail.com>
2023-01-05 10:07:19 +0800

    Surge_Qx.js 添加新格式支持

--- Surge_Qx.js
+++ Surge_Qx.js
@@ -1,3 +1,26 @@
+/*
+
+更新版本：3.0105 
+更新内容：添加更多支持类型
+
+@XiaoBL
+Qx通用格式转化Surge模块
+
+使用方法：
+添加模块或本地脚本后将 Qx 脚本链接末端添加“_surge”即可转化 Surge 格式
+
+模块链接：
+https://raw.githubusercontent.com/githubdulong/Script/master/Surge/Surge_Qx.sgmodule
+
+本地添加：
+[Script]
+格式转换 = type=http-request,pattern=_surge$,requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/githubdulong/Script/master/Surge_Qx.js
+[MITM]
+hostname = %APPEND% github.com:443, raw.githubusercontent.com:443
+
+*/
+
+
 let req = $request.url.replace(/_surge$/,'')
 let name = '#!name= ' + req.match(/.+\/(.+)\.(conf|js)/)?.[1] || '未知';
 !(async () => {
@@ -13,18 +36,20 @@ let MITM = "";
 
 body.forEach((x, y, z) => {
 	let type = x.match(
-		/script-|enabled=|reject|echo-response|\-header|hostname|url\s(302|307)|\s(request|response)-body/
+		/script-|enabled=|url\sreject|echo-response|\-header|hostname|url\s(302|307)|\s(request|response)-body/
 	)?.[0];
 	if (type) {
 		switch (type) {
 			case "script-":
 			if (x.match('echo')) {throw '未支持通用类型'}
 				z[y - 1]?.match("#") && script.push(z[y - 1]);
-				let proto = x.match('proto.js') ? ',binary-body-mode=1' : '';
+				
+let requires =	x.match('-header') ? "0" : "1";
+let proto = x.match('proto.js') ? ',binary-body-mode=1' : '' ;
 				script.push(
 					x.replace(
-						/(\^?http[^\s]+)\surl\sscript-(response|request)[^\s]+\s(http.+\/(.+)\.js)/,
-						`$4 = type=http-$2,pattern=$1,requires-body=1${proto},max-size=0,script-path=$3,script-update-interval=0`,
+						/([^\s]+)\surl\sscript-(response|request)[^\s]+\s(http.+\/(.+)\.js)/,
+						`$4 = type=http-$2,pattern=$1,requires-body=${requires}${proto},max-size=0,script-path=$3,script-update-interval=0`,
 					),
 				);
 				break;
@@ -39,24 +64,24 @@ body.forEach((x, y, z) => {
 				);
 				break;
 
-			case "reject":
-				let jct = x.match(/reject?[^\s]+/)[0];
-				let url = x.match(/\^?http[^\s]+/)?.[0];
-
-				if (jct === "reject-200") {
-					z[y - 1]?.match("#") && MapLocal.push(z[y - 1]);
-					MapLocal.push(`${url} data="https://raw.githubusercontent.com/mieqq/mieqq/master/reject-200.txt"`);
-					break;
+			case "url reject":
+			let url = x.match(/[^\s]+/)[0];
+			let jct = x.match(/reject?[^\s]+/)[0];
+				let obj = {
+				"reject-200" : 'https://raw.githubusercontent.com/mieqq/mieqq/master/reject-200.txt',
+				"reject-img" : 'https://raw.githubusercontent.com/mieqq/mieqq/master/reject-img.gif',
+				"reject-dict" : 'https://raw.githubusercontent.com/mieqq/mieqq/master/reject-dict.json',
+				"reject-array" : 'https://raw.githubusercontent.com/mieqq/mieqq/master/reject-array.json',
+			   pp : function (){
+				return this[jct]}
 				}
-
-				if (jct === "reject-img") {
+				if (obj.pp()){
 					z[y - 1]?.match("#") && MapLocal.push(z[y - 1]);
-					MapLocal.push(`${url} data="https://raw.githubusercontent.com/mieqq/mieqq/master/reject-img.gif"`);
-					break;
+					MapLocal.push(`${url} data="${obj.pp()}"`);
+				}else{
+					z[y - 1]?.match("#") && URLRewrite.push(z[y - 1]);
+				URLRewrite.push(x.replace(/([^\s]+).+/, "$1 - reject"));
 				}
-
-				z[y - 1]?.match("#") && URLRewrite.push(z[y - 1]);
-				URLRewrite.push(x.replace(/(\^?http[^\s]+).+/, "$1 - reject"));
 				break;
 
 			case "-header":
@@ -65,11 +90,11 @@ body.forEach((x, y, z) => {
 let op = x.match(/\sresponse-header/) ?
 'http-response ' : '';
      if(x.match(/\$1\$2/)){
-		  HeaderRewrite.push(x.replace(/(\^?http[^\s]+).+?n\)([^\:]+).+/,`${op}$1 header-del $2`))	
+		  HeaderRewrite.push(x.replace(/([^\s]+).+?n\)([^\:]+).+/,`${op}$1 header-del $2`))	
 		}else{
 				HeaderRewrite.push(
 					x.replace(
-						/(\^?http[^\s]+)[^\)]+\)([^:]+):([^\(]+).+\$1\s?\2?\:?([^\$]+)?\$2/,
+						/(http[^\s]+)[^\)]+\)([^:]+):([^\(]+).+\$1\s?\2?\:?([^\$]+)?\$2/,
 						`${op}$1 header-replace-regex $2 $3 $4''`,
 					),
 				);
@@ -80,8 +105,9 @@ let op = x.match(/\sresponse-header/) ?
 				break;
 
 			case "echo-response":
+			console.log(x)
 				z[y - 1]?.match("#") && MapLocal.push(z[y - 1]);
-				MapLocal.push(x.replace(/(\^?http[^\s]+).+(http.+)/, '$1 data="$2"'));
+				MapLocal.push(x.replace(/([^\s]+).+(http.+)/, '$1 data="$2"'));
 				break;
 			case "hostname":
 				MITM = x.replace(/hostname\s?=(.*)/, `[MITM]\nhostname = %APPEND% $1`);
@@ -89,7 +115,7 @@ let op = x.match(/\sresponse-header/) ?
 			default:
 				if (type.match("url ")) {
 					z[y - 1]?.match("#") && URLRewrite.push(z[y - 1]);
-					URLRewrite.push(x.replace(/(\^?http[^\s]+).+(302|307).+(http.+)/, "$1 $3 $2"));
+					URLRewrite.push(x.replace(/([^\s]+).+(302|307).+(http.+)/, "$1 $3 $2"));
 				} else {
 					z[y - 1]?.match("#") && script.push(z[y - 1]);
 					script.push(
