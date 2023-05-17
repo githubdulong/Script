f7e1b55fedde85aa14947b0c6432de609fdf7b0e
MuTu <du1238888@gmail.com>
2023-05-17 22:33:33 +0800

    ChatGPT 2.js

--- Scriptable/ChatGPT 2.js
+++ Scriptable/ChatGPT 2.js
@@ -99,7 +99,7 @@ $ui.render({
                 type: "label",
                 props: {
                     height: 20,
-                    text: "𝑱𝑺𝑩𝒐𝒙'𝑪𝒉𝒂𝒕𝑮𝑷𝑻 ⦿ 预览 ⇌ 模式",
+                    text: "𝑱𝑺𝑩𝒐𝒙'𝑪𝒉𝒂𝒕𝑮𝑷𝑻 ◉ 预览 ⇌ 模式",
                     textColor: $color("#AAAAAA"),
                     align: $align.center,
                     font: $font(10)
