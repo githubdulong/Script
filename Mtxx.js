4755f613798e211a71d9c3cbeb3dc3f918aa8d2b
MuTu <du1238888@gmail.com>
2024-04-09 15:55:35 +0800

    Mtxx.js

--- Mtxx.js
+++ Mtxx.js
@@ -23,13 +23,17 @@ hostname: api.xiuxiu.meitu.com, h5.xiuxiu.meitu.com
 
 var obj = JSON.parse($response.body);
 
-obj.data.vip_type=1;
-obj.data.expire_days=-9999999999;
-obj.data.is_expire=0;
-obj.data.in_valid_time=5576488923;
-obj.data.is_valid_user=1;
-obj.data.valid_time=5576488923;
-obj.data.home_prompt="粉钻会员 2100年1月1日到期";
-obj.data.home_btn_prompt="已解锁";
+if (obj && obj.data) {
+    obj.data.vip_type = 1;
+    obj.data.expire_days = -9999999999;
+    obj.data.is_expire = 0;
+    obj.data.in_valid_time = 5576488923;
+    obj.data.is_valid_user = 1;
+    obj.data.valid_time = 5576488923;
+    obj.data.home_prompt = "粉钻会员 2100年1月1日到期";
+    obj.data.home_btn_prompt = "已解锁";
+} else {
+    console.log("obj or obj.data is undefined");
+}
 
 $done({ body: JSON.stringify(obj) });
