47870f3a8434ddbb866e7dad1e995219e88fc3f5
MuTu <59136159+githubdulong@users.noreply.github.com>
2024-05-07 15:25:13 +0800

    Fq_Ad.js 日志输出

--- Fq_Ad.js
+++ Fq_Ad.js
@@ -1,3 +1,17 @@
+/**
+ * 模块链接: https://raw.githubusercontent.com/githubdulong/Script/master/Surge/FQ_AD.sgmodule
+ * 更新时间：2024.05.07
+ */
+
 const body = JSON.parse($response.body);
-body.resources = body.resources.filter(v => !v.url.includes("/ad"));
+console.log("原始资源列表:", JSON.stringify(body.resources));
+const filteredResources = body.resources.filter(v => {
+    const isAd = v.url.includes("/ad");
+    if (isAd) {
+        console.log("过滤广告资源:", v.url);
+    }
+    return !isAd;
+});
+body.resources = filteredResources;
+console.log("过滤后资源列表:", JSON.stringify(filteredResources));
 $done({body: JSON.stringify(body)});
