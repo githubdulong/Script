710d59a4d255f90f206041b5134fa3cff1415552
MuTu <59136159+githubdulong@users.noreply.github.com>
2024-08-21 00:06:19 +0800

    Stream-All.js

--- Stream-All.js
+++ Stream-All.js
@@ -1,7 +1,7 @@
 /*
 
 脚本参考 @Helge_0x00 
-修改日期：2024.08.20
+修改日期：2024.08.21
 Surge配置参考注释
  
  ----------------------------------------
@@ -21,16 +21,19 @@ Surge配置参考注释
  
  */
 
-const STATUS_COMING = 2
-const STATUS_AVAILABLE = 1
-const STATUS_NOT_AVAILABLE = 0
-const STATUS_TIMEOUT = -1
-const STATUS_ERROR = -2
+const STATUS_COMING = 2;
+const STATUS_AVAILABLE = 1;
+const STATUS_NOT_AVAILABLE = 0;
+const STATUS_TIMEOUT = -1;
+const STATUS_ERROR = -2;
 const UA = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36';
 const REQUEST_HEADERS = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36',
-    'Accept-Language': 'en',
-  }
+  'User-Agent': UA,
+  'Accept-Language': 'en',
+};
+const SUPPORTED_LOCATIONS = ["T1","XX","AL","DZ","AD","AO","AG","AR","AM","AU","AT","AZ","BS","BD","BB","BE","BZ","BJ","BT","BA","BW","BR","BG","BF","CV","CA","CL","CO","KM","CR","HR","CY","DK","DJ","DM","DO","EC","SV","EE","FJ","FI","FR","GA","GM","GE","DE","GH","GR","GD","GT","GN","GW","GY","HT","HN","HU","IS","IN","ID","IQ","IE","IL","IT","JM","JP","JO","KZ","KE","KI","KW","KG","LV","LB","LS","LR","LI","LT","LU","MG","MW","MY","MV","ML","MT","MH","MR","MU","MX","MC","MN","ME","MA","MZ","MM","NA","NR","NP","NL","NZ","NI","NE","NG","MK","NO","OM","PK","PW","PA","PG","PE","PH","PL","PT","QA","RO","RW","KN","LC","VC","WS","SM","ST","SN","RS","SC","SL","SG","SK","SI","SB","ZA","ES","LK","SR","SE","CH","TH","TG","TO","TT","TN","TR","TV","UG","AE","US","UY","VU","ZM","BO","BN","CG","CZ","VA","FM","MD","PS","KR","TW","TZ","TL","GB"];
+const WARP_FEATURES = ["plus", "on"];
+
 let args = getArgs();
 
 (async () => {
@@ -41,24 +44,26 @@ let args = getArgs();
   minutes = minutes > 9 ? minutes : "0" + minutes;
 
   let panel_result = {
-    title: `${args.title} | ${hour}:${minutes}` || `流媒体解锁查询 | ${hour}:${minutes}`,
+    title: `${args.title} | ${hour}:${minutes}` || `解锁检测 | ${hour}:${minutes}`,
     content: '',
-    icon: args.icon || "play.circle",
-    "icon-color": args.color || "#007aff",
-  }
+    icon: args.icon || "eye.slash.circle.fill",
+    "icon-color": args.color || "#ffb621",
+  };
 
-  let [{ region, status }] = await Promise.all([testDisneyPlus()])
+  let [{ region, status }] = await Promise.all([testDisneyPlus()]);
   let youtubeResult = await check_youtube_premium();
   let netflixResult = await check_netflix();
-  
+
   let disney_result = formatDisneyPlusResult(status, region);
   let content = `${youtubeResult} ${netflixResult} ${disney_result}`;
-  panel_result['content'] = content;
-
+  
   let traceData = await getTraceData();
-  let gptSupportStatus = traceData.loc ? "G: \u2611" : "G: \u2612";
+  let gptSupportStatus = SUPPORTED_LOCATIONS.includes(traceData.loc) ? "G: \u2611" : "G: \u2612";
 
   content += ` ${gptSupportStatus}${traceData.loc}`;
+
+  let log = `${hour}:${minutes}.${now.getMilliseconds()} 解锁检测完成：${content}`;
+  console.log(log);
 
   panel_result['content'] = content;
 
@@ -95,48 +100,48 @@ async function check_youtube_premium() {
       let option = {
         url: 'https://www.youtube.com/premium',
         headers: REQUEST_HEADERS,
-      }
+      };
       $httpClient.get(option, function (error, response, data) {
-        if (error != null || response.status !== 200) {
-          reject('Error')
-          return
+        if (error || response.status !== 200) {
+          reject('Error');
+          return;
         }
 
         if (data.indexOf('Premium is not available in your country') !== -1) {
-          resolve('Not Available')
-          return
+          resolve('Not Available');
+          return;
         }
 
-        let region = ''
-        let re = new RegExp('"countryCode":"(.*?)"', 'gm')
-        let result = re.exec(data)
-        if (result != null && result.length === 2) {
-          region = result[1]
+        let region = '';
+        let re = new RegExp('"countryCode":"(.*?)"', 'gm');
+        let result = re.exec(data);
+        if (result && result.length === 2) {
+          region = result[1];
         } else if (data.indexOf('www.google.cn') !== -1) {
-          region = 'CN'
+          region = 'CN';
         } else {
-          region = 'US'
+          region = 'US';
         }
-        resolve(region)
-      })
-    })
-  }
+        resolve(region);
+      });
+    });
+  };
 
-  let youtube_check_result = 'Y: '
+  let youtube_check_result = 'Y: ';
 
   await inner_check()
     .then((code) => {
       if (code === 'Not Available') {
-        youtube_check_result += '\u2612 |'
+        youtube_check_result += '\u2612 |';
       } else {
-        youtube_check_result += "\u2611" + code.toUpperCase() + ' |'
+        youtube_check_result += "\u2611" + code.toUpperCase() + ' |';
       }
     })
-    .catch((error) => {
-      youtube_check_result += 'N/A |'
-    })
+    .catch(() => {
+      youtube_check_result += 'N/A |';
+    });
 
-  return youtube_check_result
+  return youtube_check_result;
 }
 
 async function check_netflix() {
@@ -145,103 +150,103 @@ async function check_netflix() {
       let option = {
         url: 'https://www.netflix.com/title/' + filmId,
         headers: REQUEST_HEADERS,
-      }
+      };
       $httpClient.get(option, function (error, response, data) {
-        if (error != null) {
-          reject('Error')
-          return
+        if (error) {
+          reject('Error');
+          return;
         }
 
         if (response.status === 403) {
-          reject('Not Available')
-          return
+          reject('Not Available');
+          return;
         }
 
         if (response.status === 404) {
-          resolve('Not Found')
-          return
+          resolve('Not Found');
+          return;
         }
 
         if (response.status === 200) {
-          let url = response.headers['x-originating-url']
-          let region = url.split('/')[3]
-          region = region.split('-')[0]
-          if (region == 'title') {
-            region = 'US'
+          let url = response.headers['x-originating-url'];
+          let region = url.split('/')[3];
+          region = region.split('-')[0];
+          if (region === 'title') {
+            region = 'US';
           }
-          resolve(region)
-          return
+          resolve(region);
+          return;
         }
 
-        reject('Error')
-      })
-    })
-  }
+        reject('Error');
+      });
+    });
+  };
 
-  let netflix_check_result = 'N: '
+  let netflix_check_result = 'N: ';
 
   await inner_check(81280792)
     .then((code) => {
       if (code === 'Not Found') {
-        return inner_check(80018499)
+        return inner_check(80018499);
       }
-      netflix_check_result += '\u2611' + code.toUpperCase() + ' |'
-      return Promise.reject('BreakSignal')
+      netflix_check_result += '\u2611' + code.toUpperCase() + ' |';
+      return Promise.reject('BreakSignal');
     })
     .then((code) => {
       if (code === 'Not Found') {
-        return Promise.reject('Not Available')
+        return Promise.reject('Not Available');
       }
 
-      netflix_check_result += '⚠' + code.toUpperCase() + ' |'
-      return Promise.reject('BreakSignal')
+      netflix_check_result += '⚠' + code.toUpperCase() + ' |';
+      return Promise.reject('BreakSignal');
     })
     .catch((error) => {
       if (error === 'BreakSignal') {
-        return
+        return;
       }
       if (error === 'Not Available') {
-        netflix_check_result += '\u2612 |'
-        return
+        netflix_check_result += '\u2612 |';
+        return;
       }
-      netflix_check_result += 'N/A |'
-    })
+      netflix_check_result += 'N/A |';
+    });
 
   return netflix_check_result;
 }
 
 async function testDisneyPlus() {
   try {
-    let { region, cnbl } = await Promise.race([testHomePage(), timeout(7000)])
-    console.log(`homepage: region=${region}, cnbl=${cnbl}`)
-    let { countryCode, inSupportedLocation } = await Promise.race([getLocationInfo(), timeout(7000)])
-    console.log(`getLocationInfo: countryCode=${countryCode}, inSupportedLocation=${inSupportedLocation}`)
+    let { region, cnbl } = await Promise.race([testHomePage(), timeout(7000)]);
+    console.log(`homepage: region=${region}, cnbl=${cnbl}`);
+    let { countryCode, inSupportedLocation } = await Promise.race([getLocationInfo(), timeout(7000)]);
+    console.log(`getLocationInfo: countryCode=${countryCode}, inSupportedLocation=${inSupportedLocation}`);
 
-    region = countryCode ?? region
-    console.log("region:" + region)
+    region = countryCode ?? region;
+    console.log("region:" + region);
     // 即将登陆
     if (inSupportedLocation === false || inSupportedLocation === 'false') {
-      return { region, status: STATUS_COMING }
+      return { region, status: STATUS_COMING };
     } else {
       // 支持解锁
-      return { region, status: STATUS_AVAILABLE }
+      return { region, status: STATUS_AVAILABLE };
     }
 
   } catch (error) {
-    console.log("error:" + error)
+    console.log("error:" + error);
 
     // 不支持解锁
     if (error === 'Not Available') {
-      console.log("不支持")
-      return { status: STATUS_NOT_AVAILABLE }
+      console.log("不支持");
+      return { status: STATUS_NOT_AVAILABLE };
     }
 
     // 检测超时
     if (error === 'Timeout') {
-      return { status: STATUS_TIMEOUT }
+      return { status: STATUS_TIMEOUT };
     }
 
-    return { status: STATUS_ERROR }
+    return { status: STATUS_ERROR };
   }
 }
 
@@ -275,25 +280,25 @@ function getLocationInfo() {
           },
         },
       }),
-    }
+    };
 
     $httpClient.post(opts, function (error, response, data) {
       if (error) {
-        reject('Error')
-        return
+        reject('Error');
+        return;
       }
 
       if (response.status !== 200) {
-        console.log('getLocationInfo: ' + data)
-        reject('Not Available')
-        return
+        console.log('getLocationInfo: ' + data);
+        reject('Not Available');
+        return;
       }
 
-      data = JSON.parse(data)
+      data = JSON.parse(data);
       if (data?.errors) {
-        console.log('getLocationInfo: ' + data)
-        reject('Not Available')
-        return
+        console.log('getLocationInfo: ' + data);
+        reject('Not Available');
+        return;
       }
 
       let {
@@ -302,10 +307,10 @@ function getLocationInfo() {
           inSupportedLocation,
           location: { countryCode },
         },
-      } = data?.extensions?.sdk
-      resolve({ inSupportedLocation, countryCode, accessToken })
-    })
-  })
+      } = data?.extensions?.sdk;
+      resolve({ inSupportedLocation, countryCode, accessToken });
+    });
+  });
 }
 
 function testHomePage() {
@@ -316,29 +321,29 @@ function testHomePage() {
         'Accept-Language': 'en',
         'User-Agent': UA,
       },
-    }
+    };
 
     $httpClient.get(opts, function (error, response, data) {
       if (error) {
-        reject('Error')
-        return
+        reject('Error');
+        return;
       }
       if (response.status !== 200 || data.indexOf('Sorry, Disney+ is not available in your region.') !== -1) {
-        reject('Not Available')
-        return
+        reject('Not Available');
+        return;
       }
 
-      let match = data.match(/Region: ([A-Za-z]{2})[\s\S]*?CNBL: ([12])/)
+      let match = data.match(/Region: ([A-Za-z]{2})[\s\S]*?CNBL: ([12])/);
       if (!match) {
-        resolve({ region: '', cnbl: '' })
-        return
+        resolve({ region: '', cnbl: '' });
+        return;
       }
 
-      let region = match[1]
-      let cnbl = match[2]
-      resolve({ region, cnbl })
-    })
-  })
+      let region = match[1];
+      let cnbl = match[2];
+      resolve({ region, cnbl });
+    });
+  });
 }
 
 function timeout(delay = 5000) {
@@ -347,15 +352,6 @@ function timeout(delay = 5000) {
       reject('Timeout');
     }, delay);
   });
-}
-
-function getFlagEmoji(countryCode, flagMapping) {
-  for (let i = 0; i < flagMapping.length; i++) {
-    if (flagMapping[i][0] === countryCode) {
-      return flagMapping[i][1];
-    }
-  }
-  return '';
 }
 
 async function getTraceData() {
